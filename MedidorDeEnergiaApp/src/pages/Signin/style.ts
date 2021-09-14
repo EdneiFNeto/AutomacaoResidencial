8617e1cf7d4ead2a7d7c5e73189225c14f1f6dce
Ednei Freitas <edneifneto@gmail.com>
2021-09-14 00:05:21 -0300

    Update Android from React-Native

---
+++ MedidorDeEnergiaApp/src/pages/Signin/style.ts
@@ -0,0 +1,58 @@
+import styled from 'styled-components/native';
+import {RectButton} from 'react-native-gesture-handler';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background: #fff;
+  padding: 0 24px;
+`;
+
+export const TextWelComer = styled.Text`
+  font-size: 26px;
+  margin-bottom: 12px;
+  font-family: 'Roboto-Regular';
+`;
+export const ImageInfo = styled.Image`
+  width: 250px;
+  height: 250px;
+`;
+
+export const Input = styled.TextInput`
+  width: 100%;
+  height: 60px;
+  border-color: #c4c4c4;
+  border-bottom-width: 1px;
+`;
+
+export const ButtonLogin = styled(RectButton)`
+  padding: 12px;
+  width: 100%;
+  background: #39a5c7;
+  border-radius: 8px;
+  margin-top: 36px;
+`;
+export const TextButtonLogin = styled.Text`
+  text-align: center;
+  font-size: 18px;
+  font-family: 'Roboto-Regular';
+`;
+export const TextInfoSignUp = styled.View`
+  flex-direction: row;
+  text-align: center;
+  margin-top: 24px;
+`;
+
+export const TextInf = styled.Text`
+  font-size: 14px;
+  font-family: 'Roboto-Light';
+`;
+
+export const ButtonSigUp = styled(RectButton)``;
+
+export const TextSignUp = styled.Text`
+  font-size: 14px;
+  font-family: 'Roboto-Medium';
+  color: #39a5c7;
+`;
