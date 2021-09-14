5b20c5ee8268974e546df253e47090089fee174a
Ednei Freitas <edneifneto@gmail.com>
2021-09-14 07:49:03 -0300

    Create App React

---
+++ App-React/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    'eslint-disable-next-line @typescript-eslint/no-unused-vars': 'off',
+    'eslint-disable @typescript-eslint/no-unused-vars': 'off'
+  },
+};
