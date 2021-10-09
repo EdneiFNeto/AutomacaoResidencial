5cb63cc2aca889ea5010f0d9d0a9242e24fdd371
Ednei Freitas <edneifneto@gmail.com>
2021-10-09 15:55:04 -0300

    Update project react

---
+++ App-React/react-native.config.js
@@ -0,0 +1,12 @@
+//Copiar assts da pasta para dentro do android/Ios nativo
+//Run: yarn react-native link
+//Android => android/app/src/main
+//Ios => MyApp/info.plist
+
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  assets: ['./assets/fonts/'],
+};
---
+++ App-React/src/components/StatusBarComponent/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+
+const StatusBarComponent: React.FC = () => {
+  return <StatusBar animated={true} backgroundColor="#39a5c7" hidden={false} />;
+};
+
+export default StatusBarComponent;
